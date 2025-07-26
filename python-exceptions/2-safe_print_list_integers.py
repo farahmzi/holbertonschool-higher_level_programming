6e3f8caa10954457424b4d86312cff0806af0ca2
farahmzi <farahalmzini@gmail.com>
2025-07-26 17:17:55 +0000

    Add files

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = 0
-    for i in set(my_list):
-        result = result + i
-    return result
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
