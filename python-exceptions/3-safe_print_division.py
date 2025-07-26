575d344ccaab2f55526e983db26876daa1831901
farahmzi <farahalmzini@gmail.com>
2025-07-26 17:19:31 +0000

    Add files

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1.intersection(set_2)
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
