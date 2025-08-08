88c70ab07833d54217e9c0550d01a0d8476d17bf
farahmzi <farahalmzini@gmail.com>
2025-08-08 19:32:06 +0000

    Fix task 1: Implement Shape ABC with duck typing in shape_info function

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,38 +1,59 @@
-#!/usr/bin/env python3
-"""Shapes, Interfaces, and Duck Typing"""
-
-import math
-
-
-class Circle:
-    """Circle shape"""
-
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * self.radius ** 2
-
-    def perimeter(self):
-        return 2 * math.pi * self.radius
+#!/usr/bin/python3
+"""
+This contains code to manipulate shapes
+"""
+from abc import ABC, abstractmethod
+from math import pi
 
 
-class Rectangle:
-    """Rectangle shape"""
+class Shape(ABC):
+    """
+    represent a shape
+    """
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    @abstractmethod
+    def area(self) -> float:
+        pass
 
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    @abstractmethod
+    def perimeter(self) -> float:
+        pass
 
 
-def shape_info(shape):
-    """Print area and perimeter using duck typing"""
-    print("Area:", shape.area())
-    print("Perimeter:", shape.perimeter())
+class Circle(Shape):
+    """
+    represent a circle
+    """
 
+    def __init__(self, radius) -> None:
+        self.radius = abs(radius)
+
+    def area(self) -> float:
+        return pi * self.radius**2
+
+    def perimeter(self) -> float:
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    """
+    represent a rectangle
+    """
+
+    def __init__(self, width, height) -> None:
+        self.__width = width
+        self.__height = height
+
+    def area(self) -> float:
+        return self.__width * self.__height
+
+    def perimeter(self) -> float:
+        return 2 * self.__height + 2 * self.__width
+
+
+def shape_info(obj) -> None:
+    """
+    print shape obj info
+    """
+    print(f"Area: {obj.area()}")
+    print(f"Perimeter: {obj.perimeter()}")
