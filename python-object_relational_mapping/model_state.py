cfb85f4c5ce7b430f03dde1f69f8696aeaf6522b
farahmzi <farahalmzini@gmail.com>
2025-09-06 19:40:30 +0000

    Update model_state.py: define/adjust SQLAlchemy State model and Base

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This module defines a State class and an instance Base = declarative_base()
+Contains the class definition of a State and Base instance
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,7 +10,8 @@ Base = declarative_base()
 
 
 class State(Base):
-    """State class that inherits from Base and links to the MySQL table states"""
-    __tablename__ = 'states'
+    """State class mapped to 'states' table"""
+
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
