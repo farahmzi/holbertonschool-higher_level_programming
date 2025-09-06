8e13d0d0e3b4f3293fd17a8e3ee7484deddbaf7a
farahmzi <farahalmzini@gmail.com>
2025-09-06 18:52:13 +0000

    Add 1-filter_states.py: filter states starting with uppercase 'N' using MySQLdb

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a State class and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that inherits from Base and links to the MySQL table states"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
